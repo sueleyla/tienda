38c71b65ab1789dd41c9c02d64bb690ad03d5fe8
sueleyla <sueleylaangulo@gmail.com>
2024-05-21 14:09:20 +0200

    tienda final

---
+++ components/ProductCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        
+        <img :src="product.image" alt=""srcset="">
+        <p>{{ product.tittle }}</p>
+        <NuxtLink :to="`/products/${product.id}`">
+        <p class="btn my-4">ver detalle</p>
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+const { product } = defineProps(["product"])
+</script>
+
+<style scoped>
+img{
+    max-height: 120px;
+    max-width: 70%;
+    margin: auto;
+
+}
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="grid grid-cols-4 gap-5 mx-10">
       <div v-for="p in products">
-        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}"</NuxtLink>
+        <ProductCard :product="p"/>
       </div>
     </div>
   </div>
